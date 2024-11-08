610861526af068fc16c6b1e883ae68e6da8db2cc
Juan Felipe Alvear <juanalvear2009@gmail.com>
2024-11-08 14:40:05 -0500

    team section

--- src/data/teamData.js
+++ src/data/teamData.js
@@ -2,42 +2,42 @@
 export const teamData = [
     {
         name: "Carlos Acosta Juliao",
-        title: "Director Ejecutivo",
+        title: "Director ejecutivo, Lider de Gerencia de Territorio",
         description: "Ingeniero Electrónico graduado de la Universidad de Columbia, New York, con una destacada trayectoria en gestión pública y desarrollo urbano. Su experiencia abarca desde la administración municipal hasta la dirección de proyectos de gran envergadura que han transformado el paisaje urbano y la calidad de vida en Barranquilla. Sus áreas de expertise incluyen la gestión y desarrollo de proyectos urbanos de alto impacto, administración pública y gobernanza local, planificación y ejecución de iniciativas de sostenibilidad urbana, estrategias comerciales y de marketing en retail, y liderazgo y dirección de empresas de desarrollo regional.",
         img: "/Fotos Potencia/FOTOS SOCIOS EDITADAS/CarlosAcosta.jpg",
         backContent: "https://via.placeholder.com/300"
     },
     {
         name: "Jorge García Ibagón",
-        title: "Director Ejecutivo",
+        title: "Director Operativo, Lider de Comunicaciones",
         description: "Ingeniero Industrial con especialización en Marketing Estratégico y MBA, con amplia experiencia en gestión comercial y marketing en diversas industrias, desde el sector automotriz hasta los medios de comunicación. Sus áreas de expertise incluyen la gestión comercial y de marketing, la dirección de agencias creativas, el desarrollo de estrategias de negocios en diversos sectores, el liderazgo y gestión de equipos, así como el desarrollo de marcas y su posicionamiento en el mercado.",
         img: "/Fotos Potencia/FOTOS SOCIOS EDITADAS/JorgeGarcia.jpg",
         backContent: "https://via.placeholder.com/300"
     },
     {
         name: "Ricardo Vives Guerra",
-        title: "Socio",
+        title: "Socio, Lider de Urbanismo y Espacios Publicos",
         description: "Arquitecto con maestrías en Diseño y Planificación de Sistemas Urbanos. Destacado profesional en desarrollo inmobiliario y gestión de proyectos urbanos de gran escala. Áreas de expertise: Diseño arquitectónico y urbanismo, Planificación de sistemas urbanos, Desarrollo inmobiliario, Gestión de empresas de desarrollo regional, Innovación en arquitectura y diseño urbano.",
         img: "/Fotos Potencia/FOTOS SOCIOS EDITADAS/RicardoVives.jpg",
         backContent: "https://via.placeholder.com/300"
     },
     {
         name: "Ricardo Plata Sarabia",
-        title: "Socio",
+        title: "Socio, Lider de Desarrollo Economico",
         description: "Ingeniero Industrial con maestría en Desarrollo Económico Urbano. Experto en desarrollo económico local y gestión de eventos y espacios de gran envergadura. Áreas de expertise: Desarrollo económico urbano y regional, Gestión de centros de eventos y ferias, Coordinación de programas de desarrollo sostenible, Inversión y capital de riesgo, Políticas públicas para el desarrollo económico.",
         img: "/Fotos Potencia/FOTOS SOCIOS EDITADAS/RicardoPlata.jpg",
         backContent: "https://via.placeholder.com/300"
     },
     {
         name: "Juan M. Alvarado",
-        title: "Socio",
+        title: "Socio, Lider de Estudios e Investigaciones",
         description: "Economista con maestría y amplia experiencia en el sector público. Reconocido por su innovación en la gestión pública y su contribución al desarrollo económico y social de las regiones. Áreas de expertise: Gestión pública innovadora, Planificación urbana y regional, Manejo de crisis y emergencias (ej. COVID-19), Investigación y análisis económico, Desarrollo de políticas públicas efectivas.",
         img: "/Fotos Potencia/FOTOS SOCIOS EDITADAS/JuanAlvarado.jpg",
         backContent: "https://via.placeholder.com/300"
     },
     {
         name: "Alberto Bernal Parada",
-        title: "Socio",
+        title: "Socio, Lider de Finanzas Publicas",
         description: "Administrador de Empresas con especialización en Finanzas. Experto en gestión financiera y administrativa en diversos sectores, desde la minería hasta la construcción y el gobierno local. Áreas de expertise: Gestión financiera y administrativa, Análisis y planificación financiera, Asesoría en finanzas públicas, Gestión de proyectos de infraestructura, Optimización de procesos administrativos.",
         img: "/Fotos Potencia/FOTOS SOCIOS EDITADAS/AlbertoBernal.jpg",
         backContent: "https://via.placeholder.com/300"
@@ -45,70 +45,57 @@ export const teamData = [
     {
         name: "Diana Granados S.",
         title: "Project Manager, Asistente Ejecutvia",
-        description: "",
+        description: "Comunicadora Social y Periodista, especialista en Alta Gerencia de la Comunicación Organizacional. Experta en estrategias de comunicación y relaciones públicas para organizaciones públicas y privadas. Áreas de expertise: Estrategias de comunicación interna y externa, Relaciones públicas y manejo de imagen, Comunicación digital y redes sociales, Asesoría en comunicación para la alta gerencia, Seguimiento y comunicación de proyectos de alto impacto.",
         img: "/Fotos Potencia/FOTOS MIEMBROS POTENCIA/DianaGranados.jpg",
         backContent: "https://via.placeholder.com/300"
     },
     {
         name: "Miguel Castillo M.",
         title: "Coordinador de Proyectos",
-        description: "",
+        description: "Profesional en Gestión y Desarrollo Urbano y en Ciencia Política y Gobierno, con maestría en Desarrollo Económico Urbano. Experiencia en gestión pública local y análisis de datos para la planificación. Áreas de expertise: Desarrollo económico urbano, Gestión de servicios públicos, Análisis de datos y sistemas de información, Planificación urbana y regional, Políticas públicas para el desarrollo local.",
         img: "/Fotos Potencia/FOTOS MIEMBROS POTENCIA/MiguelCastillo.jpg",
         backContent: "https://via.placeholder.com/300"
     },
     {
         name: "Pedro De La Puente",
         title: "Analista Senior",
-        description: "",
+        description: "PhD en Ciencias Sociales y Magíster en Economía. Académico y asesor en políticas públicas con enfoque en desarrollo regional. Áreas de expertise: Diseño y evaluación de políticas públicas, Docencia universitaria en economía, Asesoría en desarrollo económico local, Análisis socioeconómico del Caribe colombiano, Investigación en historia económica y economía regional.",
         img: "/Fotos Potencia/FOTOS MIEMBROS POTENCIA/PedroDLPuente.jpg",
         backContent: "https://via.placeholder.com/300"
     },
     {
         name: "Ramon Quintero B.",
         title: "Consultor",
-        description: "",
+        description: "Economista con maestría en Dirección de Personas en las Organizaciones. Especialista en estructuración de proyectos y medición de impactos en el sector público. Áreas de expertise: Estructuración y evaluación de proyectos, Medición de impacto de políticas públicas, Gestión del talento humano, Análisis económico para la toma de decisiones, Asesoría a la alta dirección en el sector público.",
         img: "/Fotos Potencia/FOTOS MIEMBROS POTENCIA/RamonQuintero.jpg",
         backContent: "https://via.placeholder.com/300"
     },
     {
         name: "Vivian Cabanzo F.",
         title: "Consultor",
-        description: "",
+        description: "Economista y profesional en Negocios Internacionales. Especialista en investigación socioeconómica y análisis de datos para el sector público. Áreas de expertise: Análisis de datos económicos y financieros, Investigación socioeconómica, Visualización de datos, Análisis de contratos públicos, Apoyo a la toma de decisiones basada en datos.",
         img: "/Fotos Potencia/FOTOS MIEMBROS POTENCIA/VivianCabanzo.jpg",
         backContent: "https://via.placeholder.com/300"
     },
-    {
-        name: "Carlos Nicolella G.",
-        title: "Consultor",
-        description: "",
-        img: "/Fotos Potencia/FOTOS MIEMBROS POTENCIA/CarlosNicolella.jpg",
-        backContent: "https://via.placeholder.com/300"
-    },
+
     {
         name: "Luis Monroy G.",
         title: "Consultor",
-        description: "",
+        description: "Economista especialista en Evaluación de Proyectos. Experto en evaluación económica y social de proyectos, con amplia experiencia en investigación y estudios de mercado. Áreas de expertise: Evaluación económica y social de proyectos, Estudios de mercado, Estructuración de proyectos públicos y privados, Investigación socioeconómica, Cooperación nacional e internacional.",
         img: "/Fotos Potencia/FOTOS MIEMBROS POTENCIA/LuisMonroy.jpg",
         backContent: "https://via.placeholder.com/300"
     },
     {
         name: "David Blanco P.",
         title: "Consultor",
-        description: "",
+        description: "Ingeniero Civil con maestría en Vías y Transporte. Especialista en estructuración y gerencia de proyectos de infraestructura urbana estratégica. Áreas de expertise: Gerencia de proyectos de infraestructura urbana, Diseño y planificación de vías y transporte, Desarrollo de proyectos de transformación urbana, Planificación de malla vial y espacio público, Gestión de proyectos de impacto ambiental.",
         img: "/Fotos Potencia/FOTOS MIEMBROS POTENCIA/DavidBlanco.jpg",
-        backContent: "https://via.placeholder.com/300"
-    },
-    {
-        name: "Andrés Zapata B.",
-        title: "Consultor",
-        description: "",
-        img: "https://via.placeholder.com/150",
         backContent: "https://via.placeholder.com/300"
     },
     {
         name: "Karam Abuita C.",
         title: "Consultor",
-        description: "",
+        description: "Abogado especialista en Derecho Público y Derecho Urbano, con formación en prestigiosas universidades colombianas. Posee una sólida experiencia en el sector público y en el desarrollo urbano desde 2017. Áreas de expertise: Derecho público y administrativo, Derecho urbano y planificación territorial, Gestión en instituciones de educación superior, Asesoría jurídica en administración pública local, Desarrollo urbano y proyectos de infraestructura municipal.",
         img: "/Fotos Potencia/FOTOS MIEMBROS POTENCIA/KaramAmbuita.jpeg",
         backContent: "https://via.placeholder.com/300"
     }
